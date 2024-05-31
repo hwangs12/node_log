14821e90777fca5d271f6e6becdfb730255857a7
hwangs12 <sungjun94@gmail.com>
2024-05-30 22:29:54 -0400

    attempt to understand

--- README.md
+++ README.md
@@ -2,3 +2,5 @@
 
 -   Create video streaming website
 -   Do video streaming from source from mongo
+-   Need more understanding how streaming works
+-   Need to understand how piping works
--- index.js
+++ index.js
@@ -2,6 +2,8 @@ const fs = require("fs");
 const http = require("http");
 const path = require("path");
 
+let sum = 0;
+
 http.createServer(function (req, res) {
     //NOTE: This assumes your index.html file is in the
     // same location as your root application
@@ -24,14 +26,26 @@ http.createServer(function (req, res) {
         }
 
         const videoPath = "sample-30s.mp4";
+
+        // this gives you the byte size of the video in string
         const videoSize = fs.statSync(videoPath).size;
+
+        console.log("???videosize???");
+        console.log(videoSize);
+        console.log("???videosize???");
 
         // Parse Range
         // Example: "bytes=32324-"
         const CHUNK_SIZE = 10 ** 6; // 1MB
+        // how the hell is this determined?
         const start = Number(range.replace(/\D/g, ""));
+        console.log("what the hell is range???");
+        console.log(start);
+        console.log("what the hell is range???");
         const end = Math.min(start + CHUNK_SIZE, videoSize - 1);
 
+        console.log(sum);
+        sum += (end - start) / Number(videoSize);
         console.log(req.headers.range, "----", start);
         const contentLength = end - start + 1;
         const headers = {
